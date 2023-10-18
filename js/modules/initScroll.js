3366f3e6225591c7ce8423613302740bb3a94b3f
xelsrock <alex.perevosnicov@gmail.com>
2023-10-18 16:23:54 +0300

    add animation scroll

--- js/modules/initScroll.js
+++ js/modules/initScroll.js
@@ -1,7 +1,37 @@
 const navItems = document.querySelectorAll("a[href^='#']");
-const scrollArrow = document.querySelector('.preview__about');
-const subscribe = document.querySelector('.subscribe');
-const reqTable = document.querySelector('.req__table');
+
+const scrollArrayAttr = [
+  '.about__info-subtitle',
+  '.req__table',
+  '.about__info-title',
+  '.about__text',
+  '.req__subtitle',
+  '.req__title',
+  '.reviews__item-one',
+  '.reviews__item-two',
+  '.reviews__item-three',
+  '.review__title',
+  '.review__description',
+  '.subscribe__subtitle',
+  '.subscribe__description',
+  '.rule__title',
+  '.rule__accordion',
+];
+
+const findElem = (element) => {
+  return document.querySelector(element);
+};
+
+const animationScroll = (attr) => {
+  console.log('animScroll');
+
+  const elem = findElem(attr);
+  const positionElem = elem.getBoundingClientRect().top;
+
+  if (positionElem < window.innerHeight && !elem.classList.contains('scroll')) {
+    elem.classList.add('scroll');
+  }
+};
 
 export const initScroll = () => {
   navItems.forEach((item) => {
@@ -22,41 +52,11 @@ export const initScroll = () => {
     });
   });
 
-  const animationTime = 2500;
-  const framesCount = 2500;
-
-  scrollArrow.addEventListener('click', (e) => {
+  window.addEventListener('scroll', (e) => {
     e.preventDefault();
 
-    const coordY = subscribe.getBoundingClientRect().top + window.scrollY;
-    const reqTableCoord = reqTable.getBoundingClientRect().top + window.scrollY;
-
-    reqTable.style.scale = 0;
-
-    const scroller = setInterval(() => {
-      // считаем на сколько скроллить за 1 такт
-      const scrollBy = coordY / framesCount;
-
-      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-      // и дно страницы не достигнуто
-      if (
-        scrollBy > window.scrollY - coordY &&
-        window.innerHeight + window.scrollY < document.body.offsetHeight
-      ) {
-        // то скроллим на к-во пикселей, которое соответствует одному такту
-        window.scrollBy(0, scrollBy);
-
-        if (Math.trunc(reqTableCoord - window.innerHeight) < Math.trunc(window.scrollY)) {
-          if (reqTable.style.scale < 1) {
-            reqTable.style.scale = 1;
-          }
-        }
-      } else {
-        // иначе добираемся до элемента и выходим из интервала
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-      // время интервала равняется частному от времени анимации и к-ва кадров
-    }, animationTime / framesCount);
+    scrollArrayAttr.forEach((elem) => {
+      animationScroll(elem);
+    });
   });
 };
